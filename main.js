ea436c57422b5b26cd706844bb7918e1391736b8
locrin <okristian1@gmail.com>
2017-03-05 18:46:18 +0100

    html structure

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+
+var plusSession = document.querySelector('#plusSession');
+var minusSession = document.querySelector('#minusSession');
+
+var plusBreak = document.querySelector('#plusBreak');
+var minusBreak = document.querySelector('#minusBreak');
+
+
+var sessionTime = document.querySelector('.counter-timer');
+
+console.log(sessionTime.innerHTML);
