9b9108c1ce4c31fecc3a9667b632cf259b1a9b57
locrin <okristian1@gmail.com>
2017-03-06 17:50:23 +0100

    trying to add clickevents

--- main.js
+++ main.js
@@ -2,12 +2,32 @@ var plusSession = document.querySelector('#plusSession');
 var minusSession = document.querySelector('#minusSession');
 var plusBreak = document.querySelector('#plusBreak');
 var minusBreak = document.querySelector('#minusBreak');
-var sessionTime = document.querySelector('.counter-timer');
+var sessionTime = document.querySelector('#counter-timer');
 var sessionLength= document.querySelector('#session-length');
+
+var clock = document.getElementById('.counter-timer');
+var minuteSpan = document.getElementById('.minute');
+var secondSpan = document.getElementById('.second');
 
 
 var deadline = new Date(Date.parse(new Date()) + sessionLength.innerHTML*60*1000);
 
+
+function plusSessionTime() {
+  var val = sessionLength.innerHTML;
+  val++;
+  sessionLength.innerHTML = val;
+}
+
+function minusSessionTime() {
+  var val = sessionLength.innerHTML;
+  var timer = sessionTime.innerHTML;
+  val--;
+  timer--;
+  sessionLength.innerHTML = val;
+  sessionTime.innerHTML = timer;
+
+}
 
 
 // Make clock
@@ -32,19 +52,26 @@ function addZero(i) {
   }
   return i;
 }
+
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
+  var minuteSpan = clock.querySelector('.minute');
+  var secondSpan = clock.querySelector('.second');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
     t.seconds = addZero(t.seconds);
     t.minutes = addZero(t.minutes);
-    clock.innerHTML = t.minutes + ' : ' + t.seconds;
+
+    minuteSpan.innerHTML = t.minutes;
+    secondSpan.innerHTML = t.seconds;
     if (t.total <= 0) {
       clearInterval(timeinterval);
   }
 }
+//uptede clock once to avoid delay.
 updateClock();
+//start timer
 var timeinterval = setInterval(updateClock,1000);
 }
 
