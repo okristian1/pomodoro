7eb94d1d74d7794470906e6bd99e55e4e4ea5bc6
locrin <okristian1@gmail.com>
2017-03-07 22:45:22 +0100

    switching between session and break working

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 var plusSession = document.querySelector('#plusSession');
 var minusSession = document.querySelector('#minusSession');
+var sessionTime = document.querySelector('#counter-timer');
+var sessionLength = document.querySelector('#session-length');
+
 var plusBreak = document.querySelector('#plusBreak');
 var minusBreak = document.querySelector('#minusBreak');
-var sessionTime = document.querySelector('#counter-timer');
-var sessionLength= document.querySelector('#session-length');
 
+var breakLength = document.querySelector('#break-length');
 var clock = document.getElementById('.counter-timer');
 var minuteSpan = document.getElementById('.minute');
 var secondSpan = document.getElementById('.second');
 
+var counterHeader = document.querySelector('#counter-header');
+var relax = false;
 
 
-
-function plusSessionTime() {
+plusSession.onclick = function plusSessionTime() {
   var val = sessionLength.innerHTML;
   val++;
   sessionLength.innerHTML = val;
 }
 
-function minusSessionTime() {
+minusSession.onclick = function minusSessionTime() {
   var val = sessionLength.innerHTML;
-  var timer = sessionTime.innerHTML;
   val--;
-  timer--;
   sessionLength.innerHTML = val;
-  sessionTime.innerHTML = timer;
-
 }
 
+plusBreak.onclick = function plusBreakTime() {
+  var val = breakLength.innerHTML;
+  val ++;
+  breakLength.innerHTML = val;
+}
+
+minusBreak.onclick = function minusBreakTime() {
+  var val = breakLength.innerHTML;
+  val--;
+  breakLength.innerHTML = val;
+}
 
 // Make clock
 function getTimeRemaining(endtime) {
@@ -65,7 +75,20 @@ function initializeClock(id, endtime) {
     minuteSpan.innerHTML = t.minutes;
     secondSpan.innerHTML = t.seconds;
     if (t.total <= 0) {
+      if (relax === false) {
       clearInterval(timeinterval);
+      var deadline = new Date(Date.parse(new Date()) + breakLength.innerHTML*60*1000);
+      initializeClock('counter-timer', deadline);
+      counterHeader.innerHTML = "Break";
+      relax = true;
+    }
+    else {
+      var deadline = new Date(Date.parse(new Date()) + sessionLength.innerHTML*60*1000);
+      clearInterval(timeinterval);
+      initializeClock('counter-timer', deadline);
+      counterHeader.innerHTML = "Session";
+      relax = false;
+    }
   }
 }
 //update clock to avoid delay.
